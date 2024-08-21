62a5aa89553adcfabd1748f95b08131ace79a79e
toxa81 <toxa81@gmail.com>
2024-08-21 13:10:26 +0200

    add frontend

--- README.md
+++ README.md
@@ -12,4 +12,18 @@ curl -X 'PUT' \
 
 
 # DB deployment
+
+## Create docker volume
 docker volume create slurm_data
+
+## run mysql container
+docker run --name mysql_db -e MYSQL_ROOT_PASSWORD=mysecretpassword -p 3306:3306 --mount type=volume,source=slurm_data,target=/var/lib/mysql -d mysql:latest
+
+## create table
+docker exec -it mysql_db mysql -u root -p
+create database: `CREATE DATABASE slurm_data;`
+use database: `USE slurm_data;`
+create table: `CREATE TABLE vcluster (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50) NOT NULL, jsondata TEXT NOT NULL, datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL);`
+
+to remove table: `DROP TABLE IF EXISTS vcluster;`
+
---
+++ frontend.py
@@ -0,0 +1,84 @@
+import requests
+from nicegui import ui
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+def fetch_data(vcluster_name):
+    response = requests.get(f"http://148.187.151.141:8000/status/{vcluster_name}")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {}
+
+labels = ['eiger', 'todi']
+
+@ui.page('/')
+def main():
+    with ui.tabs().classes('w-full') as tabs:
+        tab_refs = [ui.tab(e) for e in labels]
+
+    with ui.tab_panels(tabs, value=tab_refs[0]).classes('w-full'):
+        for label, tab in zip(labels, tab_refs):
+            response = fetch_data(label)
+            date = response["datetime"]
+            data = response["body"]
+            with ui.tab_panel(tab):
+                with ui.column().classes('w-full items-center'):
+                    columns = [
+                        {'name': 'name', 'field' : 'name', 'label': 'Name', 'required': True, 'align': 'left'},
+                        {'name': 'value', 'field' : 'value', 'label': 'Value'}
+                    ]
+                    num_avail = data["num_nodes_allocated"] + data["num_nodes_idle"]
+                    num_unavail = data["num_nodes_total"] - num_avail
+                    rows = [
+                        {'name': 'Total number of nodes',       'value': data["num_nodes_total"]},
+                        {'name': 'Number of avaialble nodes',   'value': num_avail},
+                        {'name': 'Node availability (%)',       'value': round(100 * float(num_avail /  data["num_nodes_total"]), 2)},
+                        {'name': 'Number of allocate nodes',    'value': data["num_nodes_allocated"]},
+                        {'name': 'Number of idle nodes',        'value': data["num_nodes_idle"]},
+                        {'name': 'Number of unavailable nodes', 'value': num_unavail},
+                        {'name': 'Occupancy of avaialble nodes (%)', 'value': round(100 * float(data["num_nodes_allocated"] / num_avail), 2)},
+                        {'name': 'Number of finished jobs in the past 24H', 'value': data["num_finished_jobs"]},
+                        {'name': 'Last measurment date and time (UTC)', 'value': date}
+                    ]
+                    ui.table(columns=columns, rows=rows, row_key='name')
+                    categories = ['1','2','3-4','5-8','9-16','17-32','33-64','65-128','129-256','>256']
+                    with ui.pyplot():
+                        plt.bar(categories, data["pending_jobs"])
+                        plt.ylabel('Number of jobs')
+                        plt.xlabel('Number of nodes')
+                        plt.xticks(rotation=90)
+                        plt.title('Pending jobs')
+                        plt.subplots_adjust(bottom=0.25)
+                        plt.gca().yaxis.set_major_locator(MaxNLocator(min_n_ticks=0, integer=True))
+
+                    with ui.pyplot():
+                        plt.bar(categories, data["running_jobs"])
+                        plt.ylabel('Number of jobs')
+                        plt.xlabel('Number of nodes')
+                        plt.xticks(rotation=90)
+                        plt.title('Running jobs')
+                        plt.subplots_adjust(bottom=0.25)
+                        plt.gca().yaxis.set_major_locator(MaxNLocator(min_n_ticks=0, integer=True))
+
+#        with ui.tab_panel(one):
+#            with ui.column().classes('w-full items-center'):
+#                columns = [
+#                    {'name': 'name', 'field' : 'name', 'label': 'Name', 'required': True, 'align': 'left'},
+#                    {'name': 'value', 'field' : 'value', 'label': 'Value'}
+#                ]
+#                rows = [
+#                    {'name': 'Total number of nodes', 'value': data["num_nodes_total"]}
+#                ]
+#                ui.table(columns=columns, rows=rows, row_key='name')
+#    #        with ui.column().classes('w-full items-center'):
+#    #            with ui.card().classes('no-shadow border-[1px] w-64 h-32 justify-center items-center'):
+#    #                ui.label('See, no shadow!')
+#    #            with ui.card().classes('no-shadow border-[1px] w-64 h-32 justify-center items-center'):
+#    #                ui.label('second card')
+#        with ui.tab_panel(two):
+#            ui.label('Second tab')
+#
+
+
+ui.run()
